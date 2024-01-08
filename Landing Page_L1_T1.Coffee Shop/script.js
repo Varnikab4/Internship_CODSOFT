4d2cae875aeeeb6156c230f4da596b85660e058c
Varnikab4 <115354211+Varnikab4@users.noreply.github.com>
2024-01-08 18:38:08 +0530

    Add files via upload

---
+++ Landing Page_L1_T1.Coffee Shop/script.js
@@ -0,0 +1,11 @@
+//navigation//
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".toggleMenu"); // Corrected selector
+    const navigation = document.querySelector(".navigation"); // Corrected selector
+    toggleMenu.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
+
+
+
